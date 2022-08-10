34bcccd796885f35e3dbf068fa4b0f6c4346e9ef
yannis <yannis.ayouaz@gmail.com>
2022-08-10 14:46:08 +0200

    commit

--- contracts/SingleSwap.sol
+++ contracts/SingleSwap.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.8.14;
+pragma solidity 0.8.16;
 pragma abicoder v2;
 
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
